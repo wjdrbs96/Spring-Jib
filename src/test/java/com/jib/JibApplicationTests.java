76bd54b173a4d1009c3110c3a8a7399c251ac1a8
wjdrbd96 <wjdrbs966@naver.com>
2022-08-21 23:01:18 +0900

    init

---
+++ src/test/java/com/jib/JibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
